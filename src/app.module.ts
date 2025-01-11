5a6a5a23c8b5c953c1f17b1e5d1560aeb8be0c26
gel-o <gelogelo6403@gmail.com>
2025-01-11 21:16:37 +0800

    generated a controller api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiService } from './api/api.service';
+import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ApiController],
   providers: [AppService, ApiService],
 })
 export class AppModule {}
